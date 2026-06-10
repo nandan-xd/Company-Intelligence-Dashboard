dc8c97d6db4d7117770db6ce38e5bb42ebf22417
nandan-xd <om21jagtap@gmail.com>
2026-06-10 14:14:32 +0530

    Add a button  on error page and also make the UI better

--- app.py
+++ app.py
@@ -8,12 +8,27 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('Secret_Key')
 def get_info(cN):
     cN = cN.lower()
+    data = None
     params = { 'q': cN, 'token': os.getenv('Finnhub_API_Key') }
     response = requests.get('https://finnhub.io/api/v1/search', params=params)
-    smbl = response.json()['result'][0]['symbol']
-    params = {'function': 'OVERVIEW', 'symbol': smbl, 'apikey': os.getenv('Alpha_Vantage_API_Key')}
-    response = requests.get('https://www.alphavantage.co/query?', params=params)
-    data = response.json()
+    config = response.json()
+    smbl = config["result"][0]['symbol'] if config["result"] else None
+    if smbl is None:
+         return None
+    params = { 'symbol': smbl, 'token': os.getenv('Finnhub_API_Key') }
+    response = requests.get('https://finnhub.io/api/v1/stock/profile2', params=params)
+    config = response.json()
+    country = config['country'] if 'country' in config else None 
+    if country is None:
+         return None
+    if len(config) == 0:
+         return None
+    if country != 'US' or country != 'USA':
+        params = {'function': 'OVERVIEW', 'symbol': smbl, 'apikey': os.getenv('Alpha_Vantage_API_Key')}
+        response = requests.get('https://www.alphavantage.co/query?', params=params)
+        data = response.json()
+    else:
+         data = config
     return data
 
 @app.route('/', methods=['GET', 'POST'])
@@ -26,12 +41,13 @@ def index():
         # if companyName.lower() == 'google' or companyName.lower() == 'youtube':
         #         companyName = 'Alphabet'
         company_info = get_info(companyName)
-        # if company_info['Name'] == None:
-        #     return redirect(url_for('error'))
+        # print(company_info)
+        if company_info == None:
+            return redirect(url_for('error'))
     return render_template('base.html', company_info=company_info)
 
-# @app.route('/error')
-# def error():
-#      return "Company Not Found"
+@app.route('/error')
+def error():
+     return "Company Not Found"
 if __name__ == '__main__':
     app.run(debug=True)
