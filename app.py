482852d51e1ef282b46360f186166ba8ab78d0d4
nandan-xd <om21jagtap@gmail.com>
2026-06-14 15:09:58 +0530

    Updated requirements file

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_info(cN):
 
 class Users(db.Model):
      id = db.Column(db.Integer, primary_key=True)
-     name = db.Column(db.String(50), nullable=False, primary_key=True)
+     name = db.Column(db.String(50), nullable=False, unique=True)
      password = db.Column(db.String(100), nullable=False)
 
      def __init__(self, name, password):
